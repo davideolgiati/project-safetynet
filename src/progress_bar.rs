d4312fa927bc9773cde12080200ae48237641d5f
Davide Olgiati <davide.olgiati@hotmail.it>
2026-08-14 21:37:03 +0200

    start

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -1,11 +1,71 @@
+use std::fmt::Display;
 use std::io::{self, Write};
 
-pub fn new_progress_bar(size: usize) -> Vec<char> {
-    let mut bar: Vec<char> = vec![' '; size];
-    bar[0] = '[';
-    bar[1] = '>';
-    bar[size - 1] = ']';
-    bar
+struct Begin;
+struct Progressing;
+struct Done;
+
+
+struct ProgressBar<State = Begin> {
+    index: usize,
+    bar: Vec<char>,
+    _state: std::marker::PhantomData<State>
+}
+
+impl ProgressBar<Begin> {
+    fn new(size: usize) -> ProgressBar<Progressing> {
+        let bar = {
+            let mut bar: Vec<char> = vec![' '; size];
+            bar[0] = '[';
+            bar[1] = '>';
+            bar[size - 1] = ']';
+            bar
+        };
+
+        ProgressBar::<Progressing> { 
+            index: 0, 
+            bar,
+            _state: std::marker::PhantomData
+        }
+    }
+}
+
+impl ProgressBar<Progressing> {
+    fn update(mut self) -> Step<'static> {
+        self.bar[self.index] = '=';
+        self.bar[self.index + 1] = '>';
+        self.index += 1;
+
+        if self.index == self.bar.len() - 2 {
+            Step::Last(ProgressBar::<Last> {
+                index: self.index,
+                bar: self.bar,
+                _state: std::marker::PhantomData,
+            })
+        } else {
+            Step::Progressing(&self)
+        }
+    }
+}
+
+
+
+impl Display for ProgressBar<Begin> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\r{}", self.bar.iter().collect::<String>())
+    }
+}
+
+impl Display for ProgressBar<Progressing> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\r{}", self.bar.iter().collect::<String>())
+    }
+}
+
+impl Display for ProgressBar<Done> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\r{}\n", self.bar.iter().collect::<String>())
+    }
 }
 
 pub fn update_progress_bar(index: usize, bar: &mut [char]) {
@@ -23,16 +83,4 @@ pub fn progress_index(cnt: usize, file_count: usize, bar: &[char]) -> usize {
 
 pub fn flush_stdout() {
     if io::stdout().flush().is_ok() {}
-}
-
-pub fn display_progress_bar(bar: &[char]) {
-    let last = bar.len() - 1;
-    
-    if bar[last - 1] == '=' {
-        println!("\r{}", bar.iter().collect::<String>());
-    } else {
-        print!("\r{}", bar.iter().collect::<String>());
-    }
-
-    flush_stdout();
 }
